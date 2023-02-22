af406f3772ff8d2c4e762ab7669307d4c8c2d53b
zhaopenghai <oceanytech@gmail.com>
2023-02-22 15:24:12 +0800

    Update DataAugmentation.py

--- CVTools/DataAugmentation.py
+++ CVTools/DataAugmentation.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 from CVTools.CVTools import showim
 from GeneralTools.FileOperator import auto_make_directory, get_files_pth
-
+from imgaug import augmenters as iaa
 
 
 def stain(img, dirt_num=3):
@@ -308,3 +308,30 @@ def patchit(root_dir,dst_dir):
 #         img = cv2.imread(pth,0)#[300:1100,500:4800]
 #         cv2.imwrite(r'G:\Dataset\DIBCO2017_GT\GT-PNG/'+file_name[i]+'.png',img)
 # change2png('G:\Dataset\DIBCO2017_GT\GT')
+def iaa_aug():
+    often = lambda aug: iaa.Sometimes(0.8, aug)
+    sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+    seldom = lambda aug: iaa.Sometimes(0.2, aug)
+    aug_seq = iaa.Sequential([
+    seldom(iaa.OneOf([
+    iaa.Invert(0.1),
+    iaa.CoarsePepper(0.005, size_percent=(0, 0.005)),
+    iaa.CoarseSaltAndPepper(0.005, size_percent=(0, 0.005)),
+    ]),),
+    often(iaa.OneOf([
+                iaa.MultiplyBrightness((0.8, 1.1)),
+                iaa.LinearContrast((0.9, 1.1)),
+                iaa.Multiply((0.8, 1.1), per_channel=0.2),
+                    ])),
+    sometimes(iaa.OneOf([
+        iaa.JpegCompression(compression=(0, 50)),
+        iaa.imgcorruptlike.GaussianNoise(severity=1),
+        iaa.imgcorruptlike.ShotNoise(severity=1),
+        iaa.imgcorruptlike.ImpulseNoise(severity=1),
+        iaa.imgcorruptlike.SpeckleNoise(severity=1),
+    ])),
+    ], random_order=False) 
+    # img = np.array(img)
+    # img = aug_seq(image=img)
+    return aug_seq
+
