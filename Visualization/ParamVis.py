70364c0e641bff1e2dfb4cdb6eaa28e8dd3bae9f
Zhao-Penghai <oceanytech@gmail.com>
2023-04-05 18:14:19 +0800

    230405
    add param vis

--- Visualization/ParamVis.py
+++ Visualization/ParamVis.py
@@ -1,5 +1,7 @@
+import torch
 
-def param_groups_lrd(model, weight_decay=0.05, no_weight_decay_list=[], layer_decay=.75):
+
+def get_param_value_dict(model):
     """
     Parameter groups for layer-wise lr decay
     Following BEiT: https://github.com/microsoft/unilm/blob/master/beit/optim_factory.py#L58
@@ -7,43 +9,105 @@ def param_groups_lrd(model, weight_decay=0.05, no_weight_decay_list=[], layer_de
     param_group_names = {}
     param_groups = {}
 
-    num_layers = len(model.blocks) + 1
+    for name, param in model.named_parameters():
+        # param_groups.update({name:[torch.norm(param,p=1).item(),torch.norm(param,p=2).item(),torch.norm(param,p=float('inf')).item()]})
+        # print(name)
+        if name.startswith("block") and name.endswith('.weight'):
+            param_groups.update({name: torch.norm(param, p=2).item()})
+        # print(torch.norm(param,p=1).item())
+        # print(torch.norm(param,p=2).item())
+        # print(torch.norm(param,p=float('inf')).item())
 
-    layer_scales = list(layer_decay ** (num_layers - i) for i in range(num_layers + 1))
-
-    for n, p in model.named_parameters():
-        if not p.requires_grad:
-            continue
-
-        # no decay: all 1D parameters and model specific ones
-        if p.ndim == 1 or n in no_weight_decay_list:
-            g_decay = "no_decay"
-            this_decay = 0.
-        else:
-            g_decay = "decay"
-            this_decay = weight_decay
-
-        layer_id = get_layer_id_for_vit(n, num_layers)
-        group_name = "layer_%d_%s" % (layer_id, g_decay)
-
-        if group_name not in param_group_names:
-            this_scale = layer_scales[layer_id]
-
-            param_group_names[group_name] = {
-                "lr_scale": this_scale,
-                "weight_decay": this_decay,
-                "params": [],
-            }
-            param_groups[group_name] = {
-                "lr_scale": this_scale,
-                "weight_decay": this_decay,
-                "params": [],
-            }
-
-        param_group_names[group_name]["params"].append(n)
-        param_groups[group_name]["params"].append(p)
+        # param_group_names[group_name]["params"].append(n)
+        # param_groups[group_name]["params"].append(p)
 
     # print("parameter groups: \n%s" % json.dumps(param_group_names, indent=2))
 
-    return list(param_groups.values())
+    return param_groups
 
+
+from functools import partial
+
+import torch
+import torch.nn as nn
+
+import timm.models.vision_transformer
+
+
+class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
+    """ Vision Transformer with support for global average pooling
+    img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                 num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
+                 drop_path_rate=0., hybrid_backbone=None, norm_layer=nn.LayerNorm
+    """
+
+    def __init__(self, global_pool=False, **kwargs):
+        super(VisionTransformer, self).__init__(**kwargs)
+
+        self.global_pool = global_pool
+        if self.global_pool:
+            norm_layer = kwargs['norm_layer']
+            embed_dim = kwargs['embed_dim']
+            self.fc_norm = norm_layer(embed_dim)
+
+            del self.norm  # remove the original norm
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        if self.global_pool:
+            x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
+            outcome = self.fc_norm(x)
+        else:
+            x = self.norm(x)
+            outcome = x[:, 0]
+
+        return outcome
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+
+
+def load_model(model, weights_pth):
+    checkpoint = torch.load(weights_pth, map_location='cpu')
+    # del checkpoint['model']['decoder_pos_embed']
+    # del checkpoint['model']['pos_embed']
+    msg = model.load_state_dict(checkpoint['model'], strict=False)
+    print(msg)
+    print("Load checkpoint %s" % weights_pth)
+    return model
+
+
+weights_pth = r"C:\Users\Ocean\Downloads\mae_finetuned_vit_base.pth"
+weights2_pth = r"C:\Users\Ocean\Downloads\mae_pretrain_vit_base_full-wdd80.pth"
+weights3_pth = r"C:\Users\Ocean\Downloads\mae_pretrain_vit_base.pth"
+model = VisionTransformer(
+    patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+    norm_layer=partial(nn.LayerNorm, eps=1e-6))
+msg = load_model(model,weights_pth)
+model_2 = VisionTransformer(
+    patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+    norm_layer=partial(nn.LayerNorm, eps=1e-6))
+msg = load_model(model_2,weights2_pth)
+model_3 = VisionTransformer(
+    patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+    norm_layer=partial(nn.LayerNorm, eps=1e-6))
+msg = load_model(model_3,weights3_pth)
+# print(msg)
+p = get_param_value_dict(model)
+print(p)
+p = get_param_value_dict(model_2)
+print(p)
+p = get_param_value_dict(model_3)
+print(p)
